af30f2fa183d5ce71f216cc086e05cecbfdd1091
Janakkumar Bhanushali <jbhanush@ws.cs.ryerson.ca>
2017-10-04 20:25:30 -0400

    ball game made in game makers union

---
+++ Assets/scripts/Bounce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+	public float speed;
+	public float randomSpeedMin;
+	public float randomSpeedMax;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start (){
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Random.Range()
+		
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			rb.AddForce(0, speed, 0);
+		}
+	}
+}
---
+++ Assets/scripts/PlayerMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour {
+
+    public float speed;
+    Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tr.position += Vector3.Lerp(tr.position, new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")), speed);
+	}
+}
